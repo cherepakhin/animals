e5cd94a212984e412463eddd2b75530ce818d35d
Черепахин Василий <vasi.che@gmail.com>
2023-03-19 15:16:50 +0500

    Шпаргалка по Function

--- src/test/java/ru/perm/v/animals/controller/FunctionTest.java
+++ src/test/java/ru/perm/v/animals/controller/FunctionTest.java
@@ -25,6 +25,14 @@ public class FunctionTest {
     }
 
     @Test
+    void funcTestPointerTwoParam() {
+
+        int radix = 16;
+        Function<String, Integer> strToIntStrelka = s -> Integer.parseInt(s, radix);
+        assertEquals(255, strToIntStrelka.apply("FF"));
+    }
+
+    @Test
     void funcTestLongImplement() {
         Function<String, Integer> strToIntLongImplement = new Function<String, Integer>() {
             @Override
@@ -34,5 +42,15 @@ public class FunctionTest {
         };
 
         assertEquals(222, strToIntLongImplement.apply("222"));
+    }
+
+    @Test
+    void showFunctionAndThen_AndCompose() {
+        Function<String, String> f1 = s -> s + "1";
+        Function<String, String> f2 = s -> s + "2";
+        Function<String, String> f3 = s -> s + "3";
+        Function<String, String> f4 = s -> s + "4";
+        assertEquals("Compose4312", f1.andThen(f2).compose(f3).compose(f4).apply("Compose"));
+        assertEquals("AndThen123", f1.andThen(f2).andThen(f3).apply("AndThen"));
     }
 }
