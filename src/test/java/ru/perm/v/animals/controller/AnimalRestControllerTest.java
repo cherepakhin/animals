4bb5a1f649be56ff3d456abdebdc7ffe55983d41
Черепахин Василий <vasi.che@gmail.com>
2023-03-19 13:54:20 +0500

    Animals -> Animal

--- src/test/java/ru/perm/v/animals/controller/AnimalRestControllerTest.java
+++ src/test/java/ru/perm/v/animals/controller/AnimalRestControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AnimalRestController.class)
-class AnimalControllerTest {
+class AnimalRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
