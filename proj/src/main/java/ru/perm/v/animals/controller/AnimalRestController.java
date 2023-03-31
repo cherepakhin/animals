8c80bd73319a6dce1ea7f3974cbb06b01543a031
Черепахин Василий <vasi.che@gmail.com>
2023-03-31 18:40:37 +0500

    Обработка REST ответа, при ошибке (сущность не найденна и т.п.)

--- proj/src/main/java/ru/perm/v/animals/controller/AnimalRestController.java
+++ proj/src/main/java/ru/perm/v/animals/controller/AnimalRestController.java
@@ -1,7 +1,5 @@
 package ru.perm.v.animals.controller;
 
-import java.util.stream.Collectors;
-
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -9,12 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
+import ru.perm.v.animals.controller.exception.BadRequestException;
 import ru.perm.v.animals.dto.AnimalDto;
 import ru.perm.v.animals.model.AnimalDB;
 import ru.perm.v.animals.service.AnimalService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/animal")
@@ -31,14 +30,19 @@ public class AnimalRestController {
         return animalService
                 .getAll()
                 .stream()
-                .map(animalDB -> new AnimalDto(animalDB.getId(),animalDB.getName()))
+                .map(animalDB -> new AnimalDto(animalDB.getId(), animalDB.getName()))
                 .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
     public AnimalDto getById(@PathVariable Long id) {
         log.info("get /animal/" + id);
-        AnimalDB animal = animalService.getById(id);
-        return new AnimalDto(animal.getId(),animal.getName());
+        try {
+            AnimalDB animal = animalService.getById(id);
+            AnimalDto dto = new AnimalDto(animal.getId(), animal.getName());
+            return dto;
+        } catch (Exception e) {
+            throw new BadRequestException(String.format("Animal not found id=%d", id));
+        }
     }
 }
