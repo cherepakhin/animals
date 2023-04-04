53aa4a6c4a0a9887d90b32958122a98595c5625f
Черепахин Василий <vasi.che@gmail.com>
2023-04-04 18:16:52 +0500

    Перенос БД на localhost

--- src/main/resources/db/migration/V1_002__fill_table.sql
+++ src/main/resources/db/migration/V1_002__fill_table.sql
@@ -2,15 +2,15 @@ insert into animal(id,name) values(0,'Заяц');
 insert into animal(id,name) values(1,'Волк');
 insert into animal(id,name) values(2,'Корова');
 
-insert into country(id,name) values(0,'Россия');
-insert into country(id,name) values(1,'Германия');
+insert into country(id,name) values(100,'Россия');
+insert into country(id,name) values(200,'Германия');
 
 -- Россия
-insert into statistic(id, animal_id,country_id, qty) values(0,0,0,0);
-insert into statistic(id, animal_id,country_id, qty) values(1,1,0,10);
-insert into statistic(id, animal_id,country_id, qty) values(2,2,0,20);
+insert into statistic(id, animal_id,country_id, qty) values(0,0,100,0);
+insert into statistic(id, animal_id,country_id, qty) values(1,1,100,10);
+insert into statistic(id, animal_id,country_id, qty) values(2,2,100,20);
 -- Германия
-insert into statistic(id, animal_id,country_id, qty) values(3,0,1,30);
-insert into statistic(id, animal_id,country_id, qty) values(4,1,1,40);
-insert into statistic(id, animal_id,country_id, qty) values(5,2,1,50);
+insert into statistic(id, animal_id,country_id, qty) values(3,0,200,30);
+insert into statistic(id, animal_id,country_id, qty) values(4,1,200,40);
+insert into statistic(id, animal_id,country_id, qty) values(5,2,200,50);
 
