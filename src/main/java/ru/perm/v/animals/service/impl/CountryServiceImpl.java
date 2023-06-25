5f2675190f4381ed099318a29255f3d8fe49c507
Черепахин Василий <vasi.che@gmail.com>
2023-06-25 16:53:05 +0500

    move  CountryServiceImpl

--- src/main/java/ru/perm/v/animals/service/impl/CountryServiceImpl.java
+++ src/main/java/ru/perm/v/animals/service/impl/CountryServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.perm.v.animals.service.impl;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.perm.v.animals.model.CountryDB;
 import ru.perm.v.animals.repo.CountryRepository;
@@ -11,7 +13,7 @@ public class CountryServiceImpl implements CountryService {
 
     private CountryRepository countryRepository;
 
-    public CountryServiceImpl(CountryRepository countryRepository) {
+    public CountryServiceImpl(@Autowired CountryRepository countryRepository) {
         this.countryRepository = countryRepository;
     }
 
