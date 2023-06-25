1121758e63f325fff984b692ffb74a7864e504fe
Черепахин Василий <vasi.che@gmail.com>
2023-06-25 16:50:29 +0500

    merge

--- src/main/java/ru/perm/v/animals/service/impl/CountryServiceImpl.java
+++ src/main/java/ru/perm/v/animals/service/impl/CountryServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.perm.v.animals.service.impl;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.perm.v.animals.model.CountryDB;
 import ru.perm.v.animals.repo.CountryRepository;
@@ -10,8 +9,11 @@ import ru.perm.v.animals.service.CountryService;
 @Service
 public class CountryServiceImpl implements CountryService {
 
-    @Autowired
     private CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
 
     @Override
     public List<CountryDB> getAll() {
