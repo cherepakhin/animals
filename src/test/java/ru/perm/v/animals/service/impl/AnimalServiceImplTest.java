12ca4f4d93c7d56150a4bf8a0447d38c686a2079
Черепахин Василий <vasi.che@gmail.com>
2024-11-12 23:26:39 +0500

    disable AnimalServiceImplTest.java

--- src/test/java/ru/perm/v/animals/service/impl/AnimalServiceImplTest.java
+++ src/test/java/ru/perm/v/animals/service/impl/AnimalServiceImplTest.java
@@ -2,12 +2,15 @@ package ru.perm.v.animals.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.perm.v.animals.service.AnimalService;
 
 @SpringBootTest
+//TODO: change to mock
+@Disabled
 class AnimalServiceImplTest {
 
     @Autowired
